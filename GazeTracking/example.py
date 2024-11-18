835460e48483ae11cc0050e34e6261cc6de05fdf
dueuwh <usang0821@naver.com>
2024-11-18 20:50:38 +0900

    update handcraft branch

--- GazeTracking/example.py
+++ GazeTracking/example.py
@@ -8,7 +8,7 @@ from gaze_tracking import GazeTracking
 import numpy as np
 import time
 
-video_select = int(input("0 for IITP_눈_돌리기.mp4\n1 for IITP_고개_돌리기.mp4\n2  for webcam with rolling_eye label\n3 for 3min experiment\nany keys for without label\ninput: "))
+video_select = int(input("0 for IITP_눈_돌리기.mp4\n1 for IITP_고개_돌리기.mp4\n2  for webcam with rolling_eye label\n3 for 3min experiment\n4 for EMMA_video_2.mp4\nany keys for without label\ninput: "))
 
 gaze = GazeTracking()
 rolling_eye = "../data/IITP_눈_돌리기.mp4"
@@ -16,6 +16,7 @@ label4rolling_eye = [1 for i in range(30*30)] + [0 for i in range(3550)]
 rolling_head = "../data/IITP_고개_돌리기.mp4"
 label4rolling_head = [1 for i in range(30*29)] + [0 for i in range(3564)]
 exp_tutorial = [1 for i in range(30*30)] + [0 for i in range(30*30*4)] + [ 1 for i in range(30*30)]
+label4emma2 = [1 for i in range(30*30)] + [0 for i in range(int(30*30*3))] + [1 for i in range(397)]
 camera = 0
 
 if video_select == 0:
@@ -34,6 +35,10 @@ elif video_select == 3:
     print("start webcam with 3min test\nFirst 30sec: looking center\nSecond 30sec: Looking left\nThird 30sec: Looking right\nFourth 30sec: Looking bottom\nFifth 30sec: Looking Upper\nLast 30sec: Looking center")
     webcam = cv2.VideoCapture(0)
     label = exp_tutorial
+elif video_select == 4:
+    print("start EMMA_video_2.mp4 test")
+    webcam = cv2.VideoCapture("../data/EMMA_video_2.mp4")
+    label = label4emma2
 else:
     print("\nstart webcam without label\n")
     webcam = cv2.VideoCapture(0)
@@ -56,8 +61,10 @@ while True:
         if processed_frame_count == 30*29+2550:
             break
     # We get a new frame from the webcam
-    _, frame = webcam.read()
+    ret, frame = webcam.read()
 
+    if not ret:
+        break
     # We send this frame to GazeTracking to analyze it
     gaze.refresh(frame)
 
@@ -126,15 +133,15 @@ while True:
         min3_accuracy = "No label"
     
     elapsed_time = time.time() - start_time
-    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.2, (147, 58, 31), 2)
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 0.8, (147, 58, 31), 2)
     cv2.putText(frame, f"30 seconds concentration score: {round(sum(temp_list[-30*fps:])/(30*fps), 2)} Accuracy: {sec30_accuracy}%",
-                (90, 120), cv2.FONT_HERSHEY_DUPLEX, 0.8, (147, 58, 31), 2)
+                (90, 120), cv2.FONT_HERSHEY_DUPLEX, 0.5, (147, 58, 31), 2)
     cv2.putText(frame, f"1 minute concentration score: {round(sum(temp_list[-60*fps:])/(60*fps), 2)} Accuracy: {min1_accuracy}%",
-                (90, 160), cv2.FONT_HERSHEY_DUPLEX, 0.8, (147, 58, 31), 2)
+                (90, 160), cv2.FONT_HERSHEY_DUPLEX, 0.5, (147, 58, 31), 2)
     cv2.putText(frame, f"3 minute concentration score: {round(sum(temp_list[-180*fps:])/(180*fps), 2)} Accuracy: {min3_accuracy}%",
-                (90, 200), cv2.FONT_HERSHEY_DUPLEX, 0.8, (147, 58, 31), 2)
+                (90, 200), cv2.FONT_HERSHEY_DUPLEX, 0.5, (147, 58, 31), 2)
     cv2.putText(frame, f"Elapsed time: {int(elapsed_time//3600)}:{int(elapsed_time%3600//60)}:{int(elapsed_time%60)}",
-                (90, 300), cv2.FONT_HERSHEY_DUPLEX, 0.6, (147, 58, 31), 2)
+                (90, 300), cv2.FONT_HERSHEY_DUPLEX, 0.4, (147, 58, 31), 2)
 
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
@@ -147,8 +154,12 @@ while True:
     if cv2.waitKey(1) == 27:
         break
 if label:
+    final_check = [1 for i in range(len(temp_list)) if temp_list[i] == label[i]]
+    final_accuracy = round(sum(final_check)/(len(temp_list)), 2)*100
+    print(f"{'='*60}\nFinal accuracy: {final_accuracy}%\n{'='*60}")
+else:
     final_check = [1 for i in range(30*30*6) if temp_list[i] == label[i]]
-    final_accuracy = round(sum(final_check)/(30*30*6), 2)*100
+    final_accuracy = round(sum(final_check)/(30*30*6), 2) * 100
     print(f"{'='*60}\nFinal accuracy: {final_accuracy}%\n{'='*60}")
 webcam.release()
 cv2.destroyAllWindows()
